58cbdf23dc5c2be20c0e799190fd50349678fe38
John Mirage <28307090+john-mirage@users.noreply.github.com>
2023-05-20 13:55:48 +0200

    fix: base path

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,6 @@ import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    base: "/order-summary-component/",
     plugins: [react()],
     resolve: {
         alias: {
